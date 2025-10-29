7945208ed16fab2421c699701b0cfce2f4b4aeb1
kiran naik <kiranmdr99@gmail.com>
2025-10-29 20:04:14 +0530

    final

--- blog-frontend/script.js
+++ blog-frontend/script.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_URL = 'https://blog-0d0r.onrender.com'; // Change to your deployed backend URL
+const API_URL = 'https://blogspaz.onrender.com'; // Change to your deployed backend URL
 
 // State Management
 let currentUser = null;
